00f3f038a7d77ab2dfaa2c288d33152102e550da
krishna_dixit <rkdixit96@gmail.com>
2018-02-06 23:23:21 +0530

    Checking combinedDataFromURLs helper function return array elements to contain attribute rating

--- source/helpers.js
+++ source/helpers.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 const getDataFromURL = (url) => {
-  const promise = new Promise((resolve, reject) => {
+  const urlPromise = new Promise((resolve, reject) => {
     https.get(url, (response) => {
       response.setEncoding('UTF8');
       response.on('data', (data) => {
@@ -10,10 +10,47 @@ const getDataFromURL = (url) => {
       });
     });
   });
-  return promise;
+  return urlPromise;
 };
 
-// console.log(getDataFromURL('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/1'));
+const getRatingFromURL = (url) => {
+  const ratingPromise = new Promise((resolve, reject) => {
+    getDataFromURL(url).then((data) => {
+      const jsonRating = JSON.parse(data);
+      resolve(jsonRating.rating);
+    });
+  });
+  return ratingPromise;
+};
 
-module.exports = { getDataFromURL };
+const combineDataFromURLs = (bookURL, ratingURL) => {
+  const combinedDataPromise = new Promise((resolve, reject) => {
+    getDataFromURL(bookURL).then((data) => {
+      const jsonBookData = JSON.parse(data);
+      const bookArray = jsonBookData.books;
+      const bookArrayCopy = bookArray.slice();
+      const promiseArray = [];
+      for (let book = 0; book < bookArray.length; book += 1) {
+        promiseArray.push(getRatingFromURL(`${ratingURL}/${bookArray[book].id}`));
+      }
+      Promise.all(promiseArray).then((values) => {
+        for (let ratingInd = 0; ratingInd < values.length; ratingInd += 1) {
+          bookArrayCopy[ratingInd].rating = values[ratingInd];
+        }
+        resolve(bookArrayCopy);
+      });
+    });
+  });
+  return combinedDataPromise;
+};
+
+// getDataFromURL('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks').then((data) => {
+//   console.log(data);
+// });
+
+// combineDataFromURLs('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById').then((data) => {
+//   console.log(data);
+// });
+
+module.exports = { getDataFromURL, combineDataFromURLs };
 
